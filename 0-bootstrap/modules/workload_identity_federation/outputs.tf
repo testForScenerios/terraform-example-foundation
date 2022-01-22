ed741c0bb083a4a79be95d216c2bc7cb5f3452a8
Chris Leibl <christopher@MacBook-Pro.local>
2022-01-22 17:45:51 -0500

    fix outputs in bootstrap

--- 0-bootstrap/modules/workload_identity_federation/outputs.tf
+++ 0-bootstrap/modules/workload_identity_federation/outputs.tf
@@ -1,6 +1,6 @@
 output "wif_project_id" {
   description = "Project where service accounts and core APIs will be enabled."
-  value       = module.wif_bootstrap.project_id
+  value       = module.wif_project.project_id
 }
 
 output "workload_identity_pool_name" {
@@ -11,9 +11,4 @@ output "workload_identity_pool_name" {
 output "workload_identity_provider_name" {
   description = "Provider name"
   value       = module.gh_oidc.provider_name
-}
-
-output "gcs_bucket_tfstate" {
-  description = "Bucket used for storing terraform state for foundations pipelines in seed project."
-  value       = module.seed_bootstrap.gcs_bucket_tfstate
 }
